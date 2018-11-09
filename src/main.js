a3300000d02f0728871e6739c8bfa347e0fdeb6e
fan <1508422424@qq.com>
2018-11-09 23:36:41 +0800

    修改代码

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
-import ImagesDlg from '../components/imagesDialog.vue';
-import Vue from 'vue';
-let mountedElement = document.createElement('DIV');
-document.body.appendChild(mountedElement);
-let vm = new Vue({
-    data:{
-        show:false
-    },
-    template:`<imgs-dlg v-bind:show="show" v-on:close="onClose"></imgs-dlg>`,
-    methods:{
-        onClose:function(){
-            this.show = false;
-        }
-    },
-    components:{
-        'imgs-dlg':ImagesDlg
-    }    
-}).$mount(mountedElement);
+// import ImagesDlg from '../components/imagesDialog.vue';
+// import Vue from 'vue';
+// let mountedElement = document.createElement('DIV');
+// document.body.appendChild(mountedElement);
+// let vm = new Vue({
+//     data:{
+//         show:false
+//     },
+//     template:`<imgs-dlg v-bind:show="show" v-on:close="onClose"></imgs-dlg>`,
+//     methods:{
+//         onClose:function(){
+//             this.show = false;
+//         }
+//     },
+//     components:{
+//         'imgs-dlg':ImagesDlg
+//     }    
+// }).$mount(mountedElement);
+import styles from "../style.css";
+
+let show = false;
+let overlay = document.createElement('DIV');
+overlay.classList.add(styles.overlay);
+overlay.style.display = 'none';
+let container = document.createElement('DIV');
+container.classList.add(styles.main);
+overlay.appendChild(container);
+document.body.appendChild(overlay);
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request && request.action == "imgpack"){
-            vm.show = !vm.show;
-      }
-  });
+        if (request && request.action == "imgpack"){
+            //vm.show = !vm.show;
+            show = !show;
+            if(show){
+                overlay.style.display = 'block';
+            }else{
+                overlay.style.display = 'none';
+            }
+        }
+});
+
