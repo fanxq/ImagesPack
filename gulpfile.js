69374a20be115de167c648809b3479b2ffab3091
fan <1508422424@qq.com>
2019-02-26 23:52:08 +0800

    调整文件位置，删除无用代码

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 
 gulp.task('default', function () {
     gulp.src([
-            'background.js', 'manifest.json',
+            './src/background.js',
+            './src/manifest.json',
             './images/icon16.png',
             './images/icon48.png',
             './images/icon128.png',
-            './src/iframe/iframe.html',
             './node_modules/vue/dist/vue.min.js',
             './node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js'
         ])
